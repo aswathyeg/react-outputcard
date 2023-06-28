670088f89061140fbc36587fd50872e77fcbc19b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-06-28 20:56:07 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import UserInput from "./components/Users/UserInput";
 import UserOutput from "./components/Users/UserOutput";
 
 function App() {
-  const [input, setInput] = useState(null);
-  const enterdData = [];
+  const [input, setInput] = useState([]);
+
+  const inputChangeHandler = (uName, uAge) => {
+    setInput((prev) => {
+      return [
+        ...prev,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div className="App">
-      <UserInput />
-      <UserOutput />
+      <UserInput inputChangeHandler={inputChangeHandler} />
+      <UserOutput users={input} />
     </div>
   );
 }
--- src/components/Users/UserOutput.js
+++ src/components/Users/UserOutput.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
-const UserOutput = () => {
+const UserOutput = (props) => {
   return (
     <div>
       <Card>
         <Card.Header>Entered Details</Card.Header>
-        <Card.Body></Card.Body>
+        <Card.Body>
+          {props.users.map((userlist) => {
+            <p key={userlist.id}>{userlist.uName}</p>;
+          })}
+        </Card.Body>
       </Card>
     </div>
   );
