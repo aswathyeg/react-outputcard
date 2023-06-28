8676509ca87340a8cb7b62092cad5937506a1aa3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-06-28 14:14:56 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import UserInput from "./components/Users/UserInput";
+import UserOutput from "./components/Users/UserOutput";
 
 function App() {
+  const [input, setInput] = useState(null);
+  const enterdData = [];
   return (
     <div className="App">
       <UserInput />
+      <UserOutput />
     </div>
   );
 }
